3cd36d21295954ac9a1e2d745c78720a5b1ce391
Preeti Saxena <preetisaxena2495@gmail.com>
2024-12-25 14:30:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+resolve: {
+  alias: {
+    '@': '/src', // Example alias for src folder
+  },
+},
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
